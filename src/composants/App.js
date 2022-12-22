b9a635f18943a6f712d3cf179a79395ccf24f276
sachiyo <sachiyo.sable@gmail.com>
2022-12-22 15:55:35 +0100

    Modifié et testé

--- src/composants/App.js
+++ src/composants/App.js
@@ -1,21 +1,22 @@
-import Banner from './Banner'
-import Cart from './Cart'
-import ShoppingList from './ShoppingList'
+import Banner from './Banner';
+import Cart from './Cart';
+import ShoppingList from './ShoppingList';
 // import Recommendation from './Recommendation'
 
+
 function App() {
-    return (
-        <div>
-            <Banner />
-		    {/* <Recommendation /> */}
+	return (
+		<div>
+			<Banner />
+			{/* <Recommendation /> */}
 
-            <Cart />
-            <ShoppingList />
-
-        </div>
-    )
+			<Cart />
+			<ShoppingList />
+            {/* TEST */}
+			{/* <PlantItem id = '1' name = 'Monstra'  />     */}
+		 	
+		</div>
+	);
 }
 
-export default App
-
-
+export default App;
--- src/composants/Banner.js
+++ src/composants/Banner.js
@@ -1,5 +1,5 @@
-import '../styles/Banner.css'
-import logo from '../assets/logo.png'
+import '../styles/Banner.css';
+import logo from '../assets/logo.png';
 import Recommendation from './Recommendation';
 
 function Banner() {
@@ -7,8 +7,8 @@ function Banner() {
 
 	return (
 		<div className="lmj-banner">
-			<div className='lmj-banner-row'>
-			  <img src={logo} alt='La maison jungle' className='lmj-logo' />
+			<div>
+			<img src={logo} alt='La maison jungle' className='lmj-logo' />
             <h1 className='lmj-title'>{title}</h1>
 			<Recommendation />
 		</div>
