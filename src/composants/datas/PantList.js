9d212b35b00d7d00e696a96c56210f1d60f22efc
sachiyo <sachiyo.sable@gmail.com>
2022-12-23 16:31:19 +0100

    Ajouté import imgs mais ne founctionne pas

--- src/composants/datas/PantList.js
+++ src/composants/datas/PantList.js
@@ -1,7 +1,19 @@
+// import monstera from './assets/monstera'
+// import lyrata from '../assets/lyrata.jpg'
+// import pothos from '../assets/pothos.jpg'
+// import succulent from '../assets/succulent.jpg'
+// import olivier from '../assets/olivier.jpg'
+// import basil from '../assets/basil.jpg'
+// import mint from '../assets/mint.jpg'
+// import calathea from '../assets/calathea.jpg'
+// import cactus from '../assets/cactus.jpg'
+
+
+
 // const names = ['Monster', 'Ficus Lyrata', 'Pothos Argenté', 'Calathea', 'Olivier', 'Cactus', 'Basilique', 'Succulente', 'Menthe'];
 // const category = ['classique', 'extérieur', 'plante grasse'];
 
-const plantList = [
+export const plantList = [
     { name: "Monstera", category: "classique", id: "1", light: 2,  water: 3, cover: "monstera" },
     { name: "Ficus Lyrata", category: "classique", id: "2",light: 3, water: 1, cover: "monstera"},
     { name: "Pothos Argenté", category: "classique", id: "3",light: 1, water: 2, cover: "monstera" },
@@ -39,4 +51,4 @@ plantList.push(plant_0)
 plantList.push(plant_0)
 */
 
-export { plantList }
+export default plantList;
